96a62a41ef0ec21e6ff8db228c0f5dfa2ea6313f
unknown <mayasyafira95@gmail.com>
2022-01-18 20:18:26 +0700

    tugasakhirC2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { DivisionComponent } from './division/division.component';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AppHeaderComponent } from './app-header/app-header.component';
 
 @NgModule({
   declarations: [
@@ -19,6 +20,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
     AppFooterComponent,
     DivisionComponent,
     DashboardComponent,
+    AppHeaderComponent,
   ],
   imports: [
     BrowserModule,
